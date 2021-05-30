00984e00ef474f634870607b52074de0e351b7af
Francisco Goñi <70179393+frangoni@users.noreply.github.com>
2021-05-29 21:27:50 -0300

    DEMO

--- back/db/db.js
+++ back/db/db.js
@@ -1,10 +1,7 @@
-const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/sovietica", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).catch(err => console.log(err))
-
-
-// CHEQUEAR QUE HACE ESTOOOO!!
-
-// const db = mongoose.connection;
+const mongoose = require('mongoose');
+mongoose
+  .connect('mongodb://localhost:27017/sovietica', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .catch((err) => console.log(err));
